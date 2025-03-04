d791af60249c41cedbf14e44fea1b765e2218e08
Dung <dung.le.299231@gmail.com>
2025-03-04 17:17:00 +0700

    update common service and user-service shared library

--- source-code/common-service/src/main/java/com/devnest/common/config/Translator.java
+++ source-code/common-service/src/main/java/com/devnest/common/config/Translator.java
@@ -1,26 +1,3 @@
-//package com.devnest.common.config;
-//
-//import java.util.Locale;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.i18n.LocaleContextHolder;
-//import org.springframework.context.support.ResourceBundleMessageSource;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class Translator {
-//
-//    private static ResourceBundleMessageSource messageSource;
-//
-//    private Translator(@Autowired ResourceBundleMessageSource messageSource) {
-//        Translator.messageSource = messageSource;
-//    }
-//
-//    public static String toLocale(String msg) {
-//        Locale locale = LocaleContextHolder.getLocale();
-//        return messageSource.getMessage(msg, null, locale);
-//    }
-//}
 package com.devnest.common.config;
 
 import java.util.Locale;
